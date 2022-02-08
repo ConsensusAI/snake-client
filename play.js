2ab4f087bfee12a80ba1fe69ec30149331a44481
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-08 21:50:02 +0000

    Move connection to server to client.js and export to play.js

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
+
