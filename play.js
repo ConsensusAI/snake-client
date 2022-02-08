12aacf0ff323ae70b6858b567a704ba218e3e832
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-08 23:06:34 +0000

    Implement stdin and exit condition

--- play.js
+++ play.js
@@ -4,3 +4,19 @@ const {connect} = require('./client');
 console.log("Connecting ...");
 connect();
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function() {
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    };
+  })
+};
