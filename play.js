c384ade8549ca1e52e08b50711408753e3a2a1a1
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-08 23:13:36 +0000

    Migrate stdin code to input.js

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
 const net = require("net");
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function() {
-  stdin.on("data", (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-  })
-};
+setupInput();
