fbf74a03a68209f3310081c8cac0b777f29e7fc5
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-09 00:26:07 +0000

    Implement WASD movement

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function() {
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,12 +9,22 @@ const setupInput = function() {
   return stdin;
 };
 
-const handleUserInput = function() {
-  process.stdin.on("data", (key) => {
+const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     };
-  })
+    if (key === "w") {
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
+    }
 };
 
 module.exports = {setupInput, handleUserInput};
--- play.js
+++ play.js
@@ -3,6 +3,5 @@ const {connect} = require('./client');
 const {setupInput} = require('./input');
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
